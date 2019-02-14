a536ac1e8e1fcae0f00ba13fcfba6857de4e2be3
davepatel95 <davepatel95@gmail.com>
2019-02-14 11:19:28 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+'use strict';
 
+const API_KEY = '75ba502c93d914f57e4e196ace10f46f';
+const shared_secret = '84d016400979c15a6b086b758cffab92';
+const searchURL = '';
+
+var client_id = '23872026a5984a41980843c00a7d38a7'; // Your client id
+var client_secret = '3308540224d343c0819a8a09b9d85de1'; // Your secret
