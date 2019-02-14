9790c42fa93e76e8242721365ca54ed05b91d153
davepatel95 <davepatel95@gmail.com>
2019-02-14 11:15:59 -0800

    cleared js file for test

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-var Spotify = require('spotify-web-api-js');
-var s = new Spotify();
 
-var client_id = '23872026a5984a41980843c00a7d38a7'; // Your client id
-var client_secret = '3308540224d343c0819a8a09b9d85de1'; // Your secret
